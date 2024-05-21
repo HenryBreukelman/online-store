4381aa54bb727bd065ce9e741e5943aa908da3bc
Olisa Nweze <nwezeolisa@gmail.com>
2024-05-21 16:49:35 -0500

    nav-template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
@@ -38,7 +15,6 @@ function App() {
           <Route exact path='/product' element={<Product />} />
         </Routes>
     </main>
->>>>>>> origin/main
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,55 @@
+import { CiSearch } from "react-icons/ci";
+import { MdManageAccounts } from "react-icons/md";
+import { PiShoppingCart } from "react-icons/pi";
+import { IoLocationOutline } from "react-icons/io5";
+import { BsTelephone } from "react-icons/bs";
+import { RiArrowDropDownLine } from "react-icons/ri";
+
+function Header() {
+  return (
+    <>
+      <header className='container'>
+        <section className='flex'>
+          <div className='store-title'>
+            <h1>storeName</h1>
+          </div>
+          <div className='flex w-20 align-center mr-35'>
+            <IoLocationOutline className='header-icon' />
+            <div>
+              <p className='header-p-title'>address</p>
+              <p>123 Notre Dame Ave, Somewhere, CA</p>
+            </div>
+          </div>
+          <div className='flex w-20 align-center'>
+            <BsTelephone className='header-icon' />
+            <div>
+              <p className='header-p-title'>phone</p>
+              <p>(+0061) 3457 83 84</p>
+            </div>
+          </div>
+          <div className='flex justify-right'>
+            <CiSearch className='header-icon access-icon' />
+            <MdManageAccounts className='header-icon access-icon' />
+            <PiShoppingCart className='header-icon access-icon' />
+            <p>$0.00</p>
+          </div>
+        </section>
+        <div className='divisor'></div>
+        <nav className='flex space-between'>
+          <ul className='flex'>
+            <li>home</li>
+            <li>shop</li>
+            <li>product</li>
+            <li>pages</li>
+          </ul>
+          <div className='flex'>
+            <p className='lang-p'>english</p>
+            <RiArrowDropDownLine className='lang-icon access-icon'/>
+          </div>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default Header;

ccfb677741f855cc7bff9804744c555ac787f9de
Henry <hbreukelman20@gmail.com>
2024-05-21 21:26:55 -0500

    hero banner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,16 +1,17 @@
 
 function HeroBanner () {
   return (
-    <section className="hero-banner container">
-      <div className="center hero-text">
-        <div className="center">
-          <h2>Store Name</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, illo et neque cupiditate eos quod vel, a assumenda ipsum harum dicta ea debitis perspiciatis, beatae aut veniam
-          </p>
-          <input type="button" value="Shop" />
+    <section className="hero-banner">
+      <div className="container hero-box">
+        <div className="center hero-text">
+          <div className="center">
+            <h2>Store Name</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, illo et neque cupiditate eos quod vel, a assumenda ipsum harum dicta ea debitis perspiciatis, beatae aut veniam
+            </p>
+            <input type="button" value="Shop" />
+          </div>
         </div>
-        
       </div>
     </section>
   );

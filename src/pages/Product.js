66d6047f1d78a74bcd08371e40a9653d196bcc52
Henry <hbreukelman20@gmail.com>
2024-05-24 10:09:39 -0500

    producst

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,14 +1,21 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import ProductDisplay from "../components/ProductDisplay";
-
+import { motion } from "framer-motion";
 
 function Product () {
   return (
     <>
       <Header />
-      <ProductDisplay />
-      <Footer />
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -20 }}
+        transition={{ duration: 0.5, delay: 0.5 }}
+      >
+        <ProductDisplay />
+        <Footer />
+      </motion.div>
     </>
   );
 }

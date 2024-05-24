aafc750becb2b586ee754f40d3eb070817dc6164
Henry <hbreukelman20@gmail.com>
2024-05-23 21:05:57 -0500

    animations

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,22 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import HeroBanner from "../components/HeroBanner";
 import Items from "../components/Items";
+import { motion } from "framer-motion";
 
-function Home () {
+function Home() {
   return (
     <>
       <Header />
-      <HeroBanner />
-      <Items />
-      <Footer />
+      <motion.div
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -20 }}
+        transition={{ duration: 0.5 }}
+      >
+        <HeroBanner />
+        <Items />
+        <Footer />
+      </motion.div>
     </>
   );
 }
